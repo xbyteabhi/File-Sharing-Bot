f63221468445b9715934a366d5333406ef35bd20
EverTel <139523580+evertel@users.noreply.github.com>
2023-08-23 12:57:36 +0530

    Create cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,20 +8,16 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/Evertel/File-Sharing-Bot'>Click here</a>\n○ Channel : @TechyTel\n○ Support Group : @TechyTelSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("↩️ Back", callback_data = "home"),
                         InlineKeyboardButton("🔒 Close", callback_data = "close")
                     ]
                 ]
             )
         )
-    elif data == "home"
-        await query.START_MSG()
-  
     elif data == "close":
         await query.message.delete()
         try:
