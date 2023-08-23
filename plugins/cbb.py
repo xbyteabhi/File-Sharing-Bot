8e774449a4019aa40425876c0c6c6f458659390e
EverTel <139523580+evertel@users.noreply.github.com>
2023-08-23 13:18:40 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -8,7 +8,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/evertel/File-Sharing-Bot'>Click here</a>\n○ Channel : @TechyTel\n○ Support Group : @TechyTelSupport</b>",
+            text = f"<b>○ Creator 🧑‍💻 : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language 📄 : <code>Python3</code>\n○ Library 📚 : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code 📡 : <a href='https://github.com/evertel/File-Sharing-Bot'>Click here</a>\n○ Channel 🛡 : @TechyTel\n○ Support Group 📢 : @TechyTelSupport</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
