a5b3dca08a7920d93abb3c82aa57569768a14dca
EverTel <139523580+evertel@users.noreply.github.com>
2023-08-23 17:06:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -83,8 +83,10 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("👨‍🏫 Oꜰꜰɪᴄɪᴀʟ Yᴏᴜᴛᴜʙᴇ 👨‍🏫", href="https://youtube.com/@techytel")]
+                [
+                    InlineKeyboardButton("📢 Uᴘᴅᴀᴛᴇꜱ", href="https://t.me/techytel"),
+                    InlineKeyboardButton("😊 Aʙᴏᴜᴛ Mᴇ", callback_data = "about")
                 ]
             ]
         )
